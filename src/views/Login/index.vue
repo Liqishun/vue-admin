c300c6c3221068f70811f082aa67c3d5ce8369ca
kawhi <1091775592@qq.com>
2020-07-03 22:21:30 +0800

    构建项目、配置全局样式、路由 elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="login">
+    <el-button disabled>默认按钮</el-button>
+    <el-button type="primary" disabled>主要按钮</el-button>
+    <el-button type="success" disabled>成功按钮</el-button>
+    <el-button type="info" disabled>信息按钮</el-button>
+    <el-button type="warning" disabled>警告按钮</el-button>
+    <el-button type="danger" disabled>危险按钮</el-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 50vh;
+  background-color: aqua;
+}
+</style>
